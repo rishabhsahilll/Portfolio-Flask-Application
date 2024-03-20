cabfceaf7037d94a10d233278fbe07e804a10c7c
RISHABH KUMAR - Google CSE <161485104+rishabhsahill@users.noreply.github.com>
2024-03-20 20:58:07 +0530

    update v-3.24.7
    update v-3.24.7

--- api/index.py
+++ api/index.py
@@ -1,12 +1,14 @@
 '''
 Name:- Portfolio
 Developer:- RISHABH KUMAR
-Version:- 3.24.6
+Version:- 3.24.7
 '''
 
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 import json
 import os
+import requests
 
 app = Flask(__name__)
 
@@ -19,6 +21,16 @@ blog_file_path = os.path.join(os.path.dirname(__file__), 'blog.json')
 # Load About Json File
 with open(file=about_file_path) as f:
     data = json.load(f)
+
+def get_gender_from_name(name):
+    url = f"https://api.genderize.io?name={name}"
+    response = requests.get(url)
+    data = response.json()
+
+    if "gender" in data:
+        return data["gender"]
+    else:
+        return "Unknown"
 
 @app.route('/')
 def about():
@@ -67,15 +79,61 @@ def contact():
     }
     if request.method == 'POST':
         fullname = request.form['fullname']
-        email = request.form['email']
+        user_email = request.form['email']
         message = request.form['message']
         
-        # Here, you can process the data as needed, such as sending an email, saving to a database, etc.
-        # For example, you could print the data to the console:
-        print(f"Full Name: {fullname}, Email: {email}, Message: {message}")
+        firts_and_last_name = str(fullname).split()
+
+        firts_name = firts_and_last_name[0]
+
+        if get_gender_from_name(fullname)=='male':
+            firts_name = f"Mr. {firts_name.capitalize()}"
+        elif get_gender_from_name(firts_name)=='female':
+           firts_name = f"Mrs. {firts_name.capitalize}"
+        else:
+            firts_name = f"Hello, {fullname.capitalize()}"
+
+        # Configuration for Flask-Mail
+        app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Replace with your SMTP server
+        app.config['MAIL_PORT'] = 587  # Replace with your SMTP port (e.g., 587 for TLS)
+        app.config['MAIL_USE_TLS'] = True
+        app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Replace with your email address
+        app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Replace with your email password
+        app.config['MAIL_DEBUG'] = True
         
-        # Redirect to a thank you page or display a confirmation message
-        return "Thank you for your message!"
+        mail = Mail(app)
+        
+        # Sende Mesage On Email
+        msg = Message(subject='New Contact Form Submission: Inquiry from Portfolio - Rishabh Kumar ',
+                    sender=os.getenv('MAIL_USERNAME'),  # Replace with your email address
+                    recipients=[user_email])  # Replace with recipient's email address
+
+        # Set the body of the email
+        msg.html = f'''
+            <H2><b>{firts_name}</b></H2>,
+
+            <H3>You have received a new contact form submission from your website. Here is the summary:</H3>
+
+            <H4>
+            Name: {fullname}<br>
+            Email: {user_email}<br>
+            Message: {message}
+            </H4>
+
+            Please take necessary action or respond accordingly.<br>
+
+            Thank you for reaching out!<br><br>
+
+            Best Regards,<br>
+            <a href="https://bento.me/sahill-ray">RISHABH</a> <a href="https://bento.me/sahill-ray"> SAHIL</a><br>
+            <a href="https://github.com/rishabhsahilll">RSE</a><br>
+            Instagram ID: <a href="https://instagram.com/ll._rsy_.ll/">@ll._rsy_.ll</a><br>
+        '''
+
+        # Send the email
+        mail.send(msg)
+        # print("Email sent successfully!")
+        return render_template('about.html',data=data,style=style)
     
     # If it's a GET request, simply render the contact form template
     return render_template('contact.html',data=data,style=style)
